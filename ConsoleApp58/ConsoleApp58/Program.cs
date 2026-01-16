6d4787470ccf4caa6af5f80b368edae9a100fe67
do-do-sae <kdh171207@gmail.com>
2026-01-16 11:01:41 +0900

    연습..

---
+++ ConsoleApp58/ConsoleApp58/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp58
+{
+    public class Character
+    {
+        public virtual void Render()
+        {
+            Console.WriteLine("캐릭터");
+        }
+    }
+    public class Warrior : Character
+    {
+        public override void Render()
+        {
+            Console.WriteLine("워리어");
+        }
+    }
+    public class Mage : Warrior
+    {
+        public override void Render()
+        {
+            Console.WriteLine("마법사");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Character character1 = new Character();
+            //Character character2 = new Warrior(); //업캐스팅 부모<=자식메모리 참조
+            //Character character3 = new Mage();
+            //character1.Render();
+            //character2.Render();
+            //character3.Render();
+
+            //Mage mage = new Mage();
+            //mage.Render();
+            Character character = new Warrior(); //업캐스팅
+            //Warrior warrior = (Warrior)character; //다운캐스팅
+            //warrior.Render();
+            //is 연산자 문법
+            //if (character is Warrior)
+            //{
+            //    Warrior warrior = (Warrior)character;
+            //    warrior.Render();
+            //}
+
+            Warrior warrior = character as Warrior;
+            if (warrior != null)
+            {
+                warrior.Render();
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp59/ConsoleApp59/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp59
+{
+    //추상 클래스 상속을 받고있는 친구는 추상메서드를 상속받아서 꼭 구현해라
+    public abstract class Character //abstract 키워드{
+    {
+        public int hp;
+        public abstract void Job();
+    }
+    public class Mage : Character
+    {
+        public override void Job()
+        {
+            Console.WriteLine("마법사 선택");
+        }
+    }
+    public class Archer : Character
+    {
+        public override void Job()
+        {
+            Console.WriteLine("아처 선택");
+        }
+    }
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Character[] ch = new Character[2]; //배열준비
+            //수집형 알피지 캐릭터 두개 뽑았다.
+            ch[0] = new Mage();
+            ch[1] = new Archer();
+            //for (int i = 0; i < ch.Length; i++)
+            //{
+            //    ch[i].Job();
+            //}
+            foreach (Character c in ch)
+            {
+                c.Job();
+            }
+        }
+    }
+}
